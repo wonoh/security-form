867b0350bf56bed0e609a31e5097f53a9d54147f
지원오 <wonoh@zuminternet.com>
2019-09-06 09:11:12 +0900

    인메모리 유저 생성 설정

--- src/main/java/com/wonoh/security/form/config/SecurityConfig.java
+++ src/main/java/com/wonoh/security/form/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.wonoh.security.form.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,5 +22,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //{noop} 기본적인 패스워드 인코더
+        auth.inMemoryAuthentication()
+                .withUser("wonoh").password("{noop}123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }
