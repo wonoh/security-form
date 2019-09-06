59f7bd10b14341184f1577cd907f2802a2745710
wonoh <wonoh@zuminternet.com>
2019-09-06 20:16:47 +0900

    서비스로 요청 넘기도록 수정

--- src/main/java/com/wonoh/security/form/account/AccountController.java
+++ src/main/java/com/wonoh/security/form/account/AccountController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController{
 
-    private final AccountRepository accountRepository;
+    private final AccountService accountService;
 
-    public AccountController(AccountRepository accountRepository){
-        this.accountRepository = accountRepository;
+    public AccountController(AccountService accountService){
+        this.accountService = accountService;
     }
 
     @GetMapping(value = "/account/{role}/{username}/{password}")
     public Account createAccount(@ModelAttribute Account account){
 
-        return accountRepository.save(account);
+        return accountService.createNew(account);
 
     }
 }
