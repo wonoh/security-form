9e26160596d1e90f19e362b11b8bd3913ad8a924
wonoh <wonoh@zuminternet.com>
2019-09-07 11:17:26 +0900

    패스워드 인코더 적용

--- src/main/java/com/wonoh/security/form/account/AccountService.java
+++ src/main/java/com/wonoh/security/form/account/AccountService.java
@@ -4,15 +4,18 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AccountService(AccountRepository accountRepository){
+    public AccountService(AccountRepository accountRepository,PasswordEncoder passwordEncoder){
         this.accountRepository = accountRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -32,7 +35,7 @@ public class AccountService implements UserDetailsService {
 
     public Account createNew(Account account) {
 
-        account.encodePassword();
+        account.encodePassword(passwordEncoder);
 
         return this.accountRepository.save(account);
 
