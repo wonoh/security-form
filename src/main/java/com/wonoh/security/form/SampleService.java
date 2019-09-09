c68b201c4f91d96829774e9bf900c0c9f4adf99f
지원오 <wonoh@zuminternet.com>
2019-09-10 08:41:03 +0900

    SecurityContextHolder 에서 꺼내온 인증정보 UserDetails 객체로 변환하기

--- src/main/java/com/wonoh/security/form/SampleService.java
+++ src/main/java/com/wonoh/security/form/SampleService.java
@@ -2,11 +2,9 @@ package com.wonoh.security.form;
 
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 
 @Service
 public class SampleService {
@@ -14,9 +12,8 @@ public class SampleService {
     public void dashboard(){
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Object credentials = authentication.getCredentials();
-        boolean authenticated = authentication.isAuthenticated();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("====================");
+        System.out.println(userDetails.getUsername());
     }
 }
