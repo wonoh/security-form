7abdaeb371275b2f04c528b1dbd4ce669ed60666
지원오 <wonoh@zuminternet.com>
2019-09-11 09:05:31 +0900

    accessDecisionManager ( 인가 ) ROLE 설정 커스터마이징

--- src/main/java/com/wonoh/security/form/config/SecurityConfig.java
+++ src/main/java/com/wonoh/security/form/config/SecurityConfig.java
@@ -1,15 +1,54 @@
 package com.wonoh.security.form.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
-@Order(Ordered.LOWEST_PRECEDENCE - 15)
 //@EnableWebSecurity 는 자동설정 되기 때문에 추가할 필요없음.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //accessDecisionManager 커스터마이징
+    public AccessDecisionManager accessDecisionManager(){
+
+        // role admin 은 role user 의 상위 롤이라고 hierarchy 설정
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+
+        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+        webExpressionVoter.setExpressionHandler(handler);
+
+        List<AccessDecisionVoter<? extends Object>> voters = Collections.singletonList(webExpressionVoter);
+
+        return new AffirmativeBased(voters);
+    }
+
+    // expressionHandler 커스터마이징
+    public SecurityExpressionHandler expressionHandler(){
+
+        // role admin 은 role user 의 상위 롤이라고 hierarchy 설정
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+
+        return handler;
+
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,7 +57,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .mvcMatchers("/","/info","/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated();
+                .mvcMatchers("/user").hasRole("USER")
+                .anyRequest().authenticated()
+                .accessDecisionManager(accessDecisionManager())
+                // expressionHandler 로도 roleHierarchy 설정 가능!
+                .expressionHandler(expressionHandler());
 
         http.formLogin();
         http.httpBasic();
