763f5263e0b1888fc96f549699bc9b09324b3525
지원오 <wonoh@zuminternet.com>
2019-09-11 09:04:44 +0900

    user 페이지 , 컨트롤러 구현

--- src/main/java/com/wonoh/security/form/SampleController.java
+++ src/main/java/com/wonoh/security/form/SampleController.java
@@ -45,4 +45,10 @@ public class SampleController{
         model.addAttribute("message","Hello Admin " + principal.getName());
         return "admin";
     }
+
+    @GetMapping("/user")
+    public String user(Model model,Principal principal){
+        model.addAttribute("message","Hello user " + principal.getName());
+        return "user";
+    }
 }
