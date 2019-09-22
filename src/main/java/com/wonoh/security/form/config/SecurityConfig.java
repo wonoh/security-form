f35feb2c1973f20041ac4f4c06578cd3cd4b802f
지원오 <wonoh@zuminternet.com>
2019-09-23 08:30:01 +0900

    로그아웃 필터 successUrl 등록하기

--- src/main/java/com/wonoh/security/form/config/SecurityConfig.java
+++ src/main/java/com/wonoh/security/form/config/SecurityConfig.java
@@ -69,6 +69,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
+
         // 기본값인 ThreadLocal 을 사용하면 스레드 안에서만 공유한다.
         // MODE_INHERITABLETHREADLOCAL -> 스레드 안에서 생기는 스레드까지 컨텍스트 정보를 공유한다.
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
